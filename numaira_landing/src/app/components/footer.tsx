fa53292b4f922b2c21d59918b1006c419f4947fb
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2024-10-02 19:43:54 -0400

    Update footer.tsx

--- numaira_landing/src/app/components/footer.tsx
+++ numaira_landing/src/app/components/footer.tsx
@@ -3,34 +3,37 @@ import React from "react";
 import { Colors } from "../colors";
 const Footer = () => {
   return (
-    <div
-      className="flex justify-around items-center h-16 "
-      style={{
-        width: "100%",
-        color: Colors.neutral700,
-        backgroundColor: Colors.neutral100,
-      }}
-    >
-      <a>© 2024 Numaira</a>
+    <div>
+        {/* your code goes here qiming  */}
       <div
-        className="flex justify-center items-center"
+        className="flex justify-around items-center h-16 "
         style={{
-          display: "flex",
-          justifyContent: "center",
+          width: "100%",
+          color: Colors.neutral700,
+          backgroundColor: Colors.neutral100,
         }}
       >
-        <a
-          href="https://www.linkedin.com/company/104262671"
-          className="px-1 underline"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a>© 2024 Numaira</a>
+        <div
+          className="flex justify-center items-center"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
         >
-          LinkedIn
-        </a>
-        <div className="px-2">·</div>
-        <a href="mailto:numairaai@proton.me" className="px-1 underline">
-          numairaai@proton.me
-        </a>
+          <a
+            href="https://www.linkedin.com/company/104262671"
+            className="px-1 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <div className="px-2">·</div>
+          <a href="mailto:numairaai@proton.me" className="px-1 underline">
+            numairaai@proton.me
+          </a>
+        </div>
       </div>
     </div>
   );
