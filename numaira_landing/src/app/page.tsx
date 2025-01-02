21a81aaec3cad0428669931896b0773f3e38249c
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2025-01-01 22:05:51 -0500

    Update page.tsx

--- numaira_landing/src/app/page.tsx
+++ numaira_landing/src/app/page.tsx
@@ -211,31 +211,6 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-start text-center w-100">
-      {/* <div style={{ maxHeight: "350px", maxWidth: "600px" }}>
-        <div className="mb-5 font-semibold mt-5">Introducing Numaira</div>
-
-        <Header>Automate data entry for financial documents in 1-click</Header>
-        <div
-          style={{
-            color: Colors.brand1000,
-            fontSize: "15px",
-            fontWeight: "500",
-          }}
-          className="p-2"
-        >
-          Launching in October 2024
-        </div>
-
-        <div
-          className="flex justify-center mb-10"
-          style={{
-            color: Colors.neutral700,
-            display: "flex",
-            justifyContent: "center",
-          }}
-        ></div>
-        <Image src={landingImage} alt="Landing Image" />
-      </div> */}
 
       <SectionContainer style={{ marginTop: "80px", marginBottom: "80px"}}>
         <IntroTitle>Introducing Numaira</IntroTitle>
